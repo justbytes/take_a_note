f8be55e5ac2981f41e05e65d5cdc645b2f8dea36
Jesse Marino <jessemarino2020@gmail.com>
2022-04-28 22:29:30 -0700

    intial push to heroku

--- routes/notes.js
+++ routes/notes.js
@@ -2,13 +2,13 @@ const notesRouter = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-// GET Route for retrieving all the tips
+
 notesRouter.get('/', (req, res) => {
   console.info(`${req.method} request received for note`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+
 notesRouter.post('/', (req, res) => {
   console.info(`${req.method} Note recieved!`);
   console.log(req.body);
--- server.js
+++ server.js
@@ -6,19 +6,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
-// GET Route for feedback page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/assets/pages/notes.html'))
 });
