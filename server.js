95d18e665ad96692edb3e6879b17fb0e2981893b
Jesse Marino <jessemarino2020@gmail.com>
2022-06-16 14:59:49 -0700

    updated server with new html and api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const path = require('path');
-const apiRouter = require("./routes/route");
+const express = require("express");
+const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,23 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRouter);
+app.use("/api", apiRoutes);
+app.use(express.static("public"));
+app.use("/", htmlRoutes);
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/assets/pages/notes.html'))
-});
-
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/assets/pages/index.html'))
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening at ${PORT}`)
-});
+app.listen(PORT, () => console.log(`App listening at ${PORT}`));
